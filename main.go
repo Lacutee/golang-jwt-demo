aec333bfb83062b0ce4f5e85dc01a6a7d681645f
Ridho Muhammad <50231796+Lacutee@users.noreply.github.com>
2021-12-22 11:16:36 +0700

    create initDB()

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"golang-jwt-demo/database"
+	"golang-jwt-demo/models"
+
 	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
@@ -16,7 +19,27 @@ func InitializeRouter() {
 	router.HandleFunc("/SignIn", SignIn).Methods("POST")
 }
 
+func initDB() {
+	config :=
+		database.Config{
+			ServerName: "localhost:3306",
+			User:       "root",
+			Password:   "",
+			DB:         "go_lang_auth",
+		}
+
+	connectionString := database.GetConnectionString(config)
+	err := database.Connect(connectionString)
+	if err != nil {
+		panic(err.Error())
+	}
+	database.UserMigrate(&models.User{})
+	database.AuthMigrate(&models.Authentication{})
+	database.TokenMigrate(&models.Token{})
+}
+
 func main() {
+	initDB()
 	CreateRouter()
 	InitializeRouter()
 }
